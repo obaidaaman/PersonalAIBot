325bcfa72e4594adc71e500f9c8a6cd8fc26f314
obaidaaman <amanobaidofficial01@gmail.com>
2025-11-02 18:07:16 +0530

    req.txt changes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from agent import query_rag_agent
+from fastapi.middleware.cors import CORSMiddleware
 from models.chat_models import ChatModel
 load_dotenv()
 
@@ -10,7 +11,13 @@ app = FastAPI(
     description="API to interact with the RAG agent for querying information about Aman Obaid.",
     version="1.0.0"
 )
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/query", summary="Query the RAG Agent", description="Send a question to the RAG agent and receive a response.")
 def query_agent_endpoint(request: ChatModel):
